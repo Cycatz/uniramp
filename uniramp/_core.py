22cf16a8b3c0b246d8af1cb14761cd188e8a82b2
Cycatz-nycu <cycatz.cs08@nycu.edu.tw>
2021-12-05 18:35:10 +0800

    [Python] Implement basic ramp printing function

--- uniramp/_core.py
+++ uniramp/_core.py
@@ -1,21 +1,28 @@
+import argparse
+import pathlib
 from ._uniramp import Typeface
+
 
 __all__ = [
     'main'
 ]
 
 
+def print_ramp(font, character_set):
+    tf = Typeface(str(font))
+    if character_set is None or len(character_set) == 0:  
+        character_set = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ramp = sorted([(c, tf.load_glyph(ord(c))) for c in character_set], key=lambda x: x[1], reverse=True)
+
+    print('Ramp: ' + ''.join([c[0] for c in ramp]))
+
+
 def main():
-    # tf = Typeface("/home/cycatz/.local/share/fonts/Apple_Fonts/Arial/Arial.ttf")
-    tf = Typeface("/home/cycatz/.local/share/fonts/Inconsolata_git/Inconsolata-Regular.otf")
-    print(tf)
-    print(tf.num_glyph())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--font', type=pathlib.Path, required=True,
+                        help='specify the font file')
+    parser.add_argument('-c', '--character-set', type=str,
+                        help='specify the character set (default is [a-z][A-Z])')
 
-    # for c in range(ord('A'), ord('Z') + 1):
-    #     print('Current character: ', chr(c))
-    #     tf.load_glyph(c)
-
-
-    for c in '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft':
-        print('Current character: ', c)
-        print(tf.load_glyph(ord(c)))
+    args = parser.parse_args()
+    print_ramp(str(args.font), args.character_set)
