6a5d81199bc34e62fc8da5039c5f3b151c04c00c
Cycatz-nycu <cycatz.cs08@nycu.edu.tw>
2021-12-05 17:08:01 +0800

    Implement typeface class header

---
+++ include/uniramp/typeface.hpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2021, Cycatz <cycatz@staque.xyz>
+ * BSD-style license; see COPYING
+ */
+
+#pragma once
+
+#include <string>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+
+namespace uniramp {
+class Typeface
+{
+public:
+    Typeface(const std::string &font_path);
+    ~Typeface();
+    FT_Long num_glyph() { return face->num_glyphs; }
+    void load_glyph(FT_ULong charcode);
+
+private:
+    void print_glyph_metrics(FT_Glyph_Metrics *metrics);
+    double calculate_ratio(FT_Bitmap *bitmap, int max_size);
+    FT_Library library; 
+    FT_Face face;
+
+
+    FT_Int char_pixel_size; 
+};
+} /* end namespace uniramp */
+
---
+++ include/uniramp/uniramp.hpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2021, Cycatz <cycatz@staque.xyz>
+ * BSD-style license; see COPYING
+ */
+
+#pragma once
+
+#include <uniramp/typeface.hpp>
