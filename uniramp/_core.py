bf6c03d19c86fceb4f2b27ac7f7e14edfab476a1
Cycatz-nycu <cycatz.cs08@nycu.edu.tw>
2021-12-06 09:41:55 +0800

    Add the img2text script

--- uniramp/_core.py
+++ uniramp/_core.py
@@ -1,6 +1,8 @@
 import argparse
 import pathlib
 from ._uniramp import Typeface
+from .img2text import img2text
+
 
 
 __all__ = [
@@ -8,21 +10,32 @@ __all__ = [
 ]
 
 
-def print_ramp(font, character_set):
+def get_ramp(font, character_set):
     tf = Typeface(str(font))
     if character_set is None or len(character_set) == 0:  
         character_set = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     ramp = sorted([(c, tf.get_coverage(ord(c))) for c in character_set], key=lambda x: x[1], reverse=True)
 
-    print('Ramp: ' + ''.join([c[0] for c in ramp]))
+    return ramp
+
+
+def image_to_text(image, ramp):
+    img2text(image, ramp)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--font', type=pathlib.Path, required=True,
                         help='specify the font file')
+    parser.add_argument('-i', '--image', type=pathlib.Path,
+                        help='specify the image')
     parser.add_argument('-c', '--character-set', type=str,
                         help='specify the character set (default is [a-z][A-Z])')
 
     args = parser.parse_args()
-    print_ramp(str(args.font), args.character_set)
+
+    ramp = get_ramp(str(args.font), args.character_set)
+    print('Ramp: ' + ''.join([c[0] for c in ramp]))
+
+    if args.image:
+        image_to_text(str(args.image), ramp[::-1])
---
+++ uniramp/img2text.py
@@ -0,0 +1,60 @@
+# Ref: https://github.com/RameshAditya/asciify/blob/master/asciify.py
+
+from PIL import Image
+
+__all__ = [
+    'img2text'
+]
+
+'''
+method resize():
+    - takes as parameters the image, and the final width
+    - resizes the image into the final width while maintaining aspect ratio
+'''
+def resize(image, new_width=200):
+    (old_width, old_height) = image.size
+    aspect_ratio = float(old_height)/float(old_width)
+    new_height = int(aspect_ratio * new_width)
+    new_dim = (new_width, new_height)
+    new_image = image.resize(new_dim)
+    return new_image
+'''
+method grayscalify():
+    - takes an image as a parameter
+    - returns the grayscale version of image
+'''
+def grayscalify(image):
+    return image.convert('L')
+
+'''
+method modify():
+    - replaces every pixel with a character whose intensity is similar
+'''
+def modify(image, ramp):
+    # Assume the gray level = 2 ^ 8 = 256
+    GRAY_LEVEL = 256
+
+    pixels = list(image.getdata())
+    ramp_len = len(ramp)
+    new_pixels = [ramp[int(pixel_val / GRAY_LEVEL * ramp_len)][0] for pixel_val in pixels]
+    return ''.join(new_pixels)
+
+'''
+method convert():
+    - does all the work by calling all the above functions
+'''
+def convert(image, ramp, new_width=200):
+    image = resize(image)
+    image = grayscalify(image)
+    pixels = modify(image, ramp)
+    len_pixels = len(pixels)
+
+    # Construct the image from the character list
+    new_image = [pixels[index:index+new_width] for index in range(0, len_pixels, new_width)]
+    return '\n'.join(new_image)
+
+
+def img2text(path, ramp):
+    with Image.open(path) as image:
+        new_image = convert(image, ramp)
+        print(new_image)
