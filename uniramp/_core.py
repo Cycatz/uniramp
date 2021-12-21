0bd557d40d52cd4a44bc733d6164257f3d7e64b0
Cycatz-nycu <cycatz.cs08@nycu.edu.tw>
2021-12-22 02:47:21 +0800

    Move ramp functions into a single file

--- uniramp/_core.py
+++ uniramp/_core.py
@@ -1,7 +1,7 @@
 import argparse
 import pathlib
-from ._uniramp import Typeface
 from .img2text import img2text
+from .ramp import Ramp
 
 
 
@@ -9,33 +9,44 @@ __all__ = [
     'main'
 ]
 
+class Uniramp:
+    def __init__(self):
+        pass
 
-def get_ramp(font, character_set):
-    tf = Typeface(str(font))
-    if character_set is None or len(character_set) == 0:  
-        character_set = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    ramp = sorted([(c, tf.get_coverage(ord(c))) for c in character_set], key=lambda x: x[1], reverse=True)
+    def ramp(self, args):
+        ramp = Ramp(args)
+        ramp.show_ramp()
 
-    return ramp
+    def image(self, args):
+        r = Ramp(args)
+        ramp = r.get_ramp()
+        img2text(str(args.image), ramp[::-1])
 
+    def parse(self):
+        parser = argparse.ArgumentParser()
+        subparser = parser.add_subparsers()
 
-def image_to_text(image, ramp):
-    img2text(image, ramp)
+        parser_ramp = subparser.add_parser('ramp', help='Print ramp infomation')
+        parser_ramp.add_argument('-f', '--font', type=pathlib.Path, required=True,
+                                help='specify the font file')
+        parser_ramp.add_argument('-c', '--character-set', type=str,
+                            help='specify the character set (default is [a-z][A-Z])')
+        parser_ramp.add_argument('-g', '--graph', action="store_true", help='Print ramp graph')
+        parser_ramp.set_defaults(func=self.ramp)
 
+        parser_image = subparser.add_parser('image', help='Convert an image to chars')
+        parser_image.add_argument('-i', '--image', type=pathlib.Path, required=True,
+                                help='specify the image')
+        parser_image.add_argument('-f', '--font', type=pathlib.Path, required=True,
+                                help='specify the font file')
+        parser_image.add_argument('-c', '--character-set', type=str,
+                            help='specify the character set (default is [a-z][A-Z])')
+        parser_image.set_defaults(func=self.image)
 
+        args = parser.parse_args()
+        args.func(args)
+
+        
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--font', type=pathlib.Path, required=True,
-                        help='specify the font file')
-    parser.add_argument('-i', '--image', type=pathlib.Path,
-                        help='specify the image')
-    parser.add_argument('-c', '--character-set', type=str,
-                        help='specify the character set (default is [a-z][A-Z])')
-
-    args = parser.parse_args()
-
-    ramp = get_ramp(str(args.font), args.character_set)
-    print('Ramp: ' + ''.join([c[0] for c in ramp]))
-
-    if args.image:
-        image_to_text(str(args.image), ramp[::-1])
+    uniramp = Uniramp()
+    uniramp.parse()
