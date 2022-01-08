b4d3984d9fbf4d34c1fa7287b19af9f02ce869ea
Cycatz-nycu <cycatz.cs08@nycu.edu.tw>
2022-01-08 17:27:51 +0800

    Add test_coverage script and a test font

---
+++ tests/test_coverage.py
@@ -0,0 +1,6 @@
+import uniramp
+
+def test_loadfont():
+    t = uniramp._uniramp.Typeface("./NotoSansCJK-Regular.ttc")
+    assert t.get_family_name() == "Noto Sans CJK JP"
+    assert t.get_style_name() == "Regular"
