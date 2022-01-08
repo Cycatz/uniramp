26ec11881ae072246d9af3d3f477a18ed235e716
Cycatz-nycu <cycatz.cs08@nycu.edu.tw>
2022-01-09 05:15:28 +0800

    Fix some string into constants

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -1,19 +1,21 @@
 import uniramp
 import math
 
+TEST_FONT = "tests/NotoSansCJK-Regular.ttc"
 
 def is_equal(x: float, y: float, eps=1e-3):
     return math.isclose(x, y, abs_tol=eps)
 
 
 def test_coverage_ascii():
-    t = uniramp._uniramp.Typeface("tests/NotoSansCJK-Regular.ttc")
+    t = uniramp._uniramp.Typeface(TEST_FONT)
 
     assert is_equal(t.get_coverage(ord('a')), 0.199)
     assert is_equal(t.get_coverage(ord('b')), 0.237)
 
 def test_coverage_CJK():
-    t = uniramp._uniramp.Typeface("tests/NotoSansCJK-Regular.ttc")
+    t = uniramp._uniramp.Typeface(TEST_FONT)
 
     assert is_equal(t.get_coverage(ord('一')), 0.074)
     assert is_equal(t.get_coverage(ord('二')), 0.132)
+
--- tests/test_loadfont.py
+++ tests/test_loadfont.py
@@ -1,21 +1,23 @@
 import uniramp
 import pytest
 
+TEST_FONT = "tests/NotoSansCJK-Regular.ttc"
+
 
 def test_loadfont():
-    t = uniramp._uniramp.Typeface("tests/NotoSansCJK-Regular.ttc")
+    t = uniramp._uniramp.Typeface(TEST_FONT)
     assert t.get_family_name() == "Noto Sans CJK JP"
     assert t.get_style_name() == "Regular"
 
 
 def test_loadfont_with_valid_index():
-    t = uniramp._uniramp.Typeface("tests/NotoSansCJK-Regular.ttc", 3)
+    t = uniramp._uniramp.Typeface(TEST_FONT, 3)
     assert t.get_family_name() == "Noto Sans CJK TC"
     assert t.get_style_name() == "Regular"
 
 
 def test_loadfont_with_invalid_index():
     with pytest.raises(ValueError):
-        t = uniramp._uniramp.Typeface("tests/NotoSansCJK-Regular.ttc", -1)
+        t = uniramp._uniramp.Typeface(TEST_FONT, -1)
     with pytest.raises(ValueError):
-        t = uniramp._uniramp.Typeface("tests/NotoSansCJK-Regular.ttc", 100)
+        t = uniramp._uniramp.Typeface(TEST_FONT, 100)
